8c638c9b94d5b1185a6c0daaeb3c6c9836c94052
kshitij01042002 <kshitijpatil1098@gmail.com>
2024-02-16 16:20:45 +0530

    agreement

--- frontend/src/layouts/dashboard/index.js
+++ frontend/src/layouts/dashboard/index.js
@@ -50,46 +50,63 @@ import { useEffect, useState } from "react";
 import { db } from "layouts/authentication/firebase";
 import { onValue, ref } from "firebase/database";
 
-import InvoiceModal from './components/Agreement/Agreement'
+import InvoiceModal from "./components/Agreement/Agreement";
 
 function Dashboard() {
   const { gradients } = colors;
   const { cardContent } = gradients;
 
-  const[income, setIncome] =useState(null)
-  const[points, setPoints] =useState(null)
-  const[expense, setExpense] = useState(null)
+  const [income, setIncome] = useState(null);
+  const [points, setPoints] = useState(null);
+  const [expense, setExpense] = useState(null);
 
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let monthIndex = (new Date().getMonth());
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let monthIndex = new Date().getMonth();
   let monthName = monthNames[monthIndex];
-  var today = new Date()
-  var date = (today.getDate() + monthName + today.getFullYear())
+  var today = new Date();
+  var date = today.getDate() + monthName + today.getFullYear();
 
   const rewards = doc(fs, "kshitij", "details");
-  const balanceref = doc(fs, "kshitij", monthName + " Expense")
-  const[linedata, setlinedata] = useState(null)
+  const balanceref = doc(fs, "kshitij", monthName + " Expense");
+  const [linedata, setlinedata] = useState(null);
 
   const onSubmit = () => {
     axios.post("http://localhost:4000/forecast-spending").then((e) => {
-      setlinedata(e.data)
-    })
- }
+      setlinedata(e.data);
+    });
+  };
 
   useEffect(() => {
-    getDoc(rewards).then((data) => {
-      setPoints(data.data().rewards)
-    }).catch((error) => {
-      console.log("Error getting documents: ", error);
-    });
+    getDoc(rewards)
+      .then((data) => {
+        setPoints(data.data().rewards);
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
 
-    getDoc(balanceref).then((data) => {
-      setIncome(data.data().income)
-      setExpense(data.data().expense)
-    }).catch((error) => {
-      console.log("Error getting documents: ", error);
-    });
-  },[])
+    getDoc(balanceref)
+      .then((data) => {
+        setIncome(data.data().income);
+        setExpense(data.data().expense);
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+  }, []);
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
@@ -105,27 +122,21 @@ function Dashboard() {
 
   const print123 = () => {
     setIsOpen(true);
-  }
+  };
 
-  const [isOpen, setIsOpen] = useState(false); 
-  const closeModal = (event) => this.setState({isOpen: false});
-  const [states, setStates] = useState([])
-
+  const [isOpen, setIsOpen] = useState(false);
+  const closeModal = (event) => this.setState({ isOpen: false });
+  const [states, setStates] = useState([]);
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <div>
         {Object.values(projects).map((project) => (
-  <div>{project.name}</div>
-))}
-
-{
-  console.log(projects)
-}
-
-</div>
-<InvoiceModal showModal={isOpen} closeModal={closeModal} info={states}/>
+          <div>{project.name}</div>
+        ))}
+        {console.log(projects)}
+      </div>
       <VuiBox py={3}>
         <VuiBox mb={3}>
           <Grid container spacing={3}>
@@ -133,21 +144,30 @@ function Dashboard() {
               <MiniStatisticsCard
                 title={{ text: "Your Terra Points", fontWeight: "regular" }}
                 count={{ color: "success", text: points }}
-                icon={{ color: "info", component: <IoWallet size="22px" color="white" /> }}
+                icon={{
+                  color: "info",
+                  component: <IoWallet size="22px" color="white" />,
+                }}
               />
             </Grid>
             <Grid item xs={12} md={6} xl={3}>
               <MiniStatisticsCard
                 title={{ text: "This month's Income" }}
                 count={{ color: "white", text: income }}
-                icon={{ color: "info", component: <IoGlobe size="22px" color="white" /> }}
+                icon={{
+                  color: "info",
+                  component: <IoGlobe size="22px" color="white" />,
+                }}
               />
             </Grid>
             <Grid item xs={12} md={6} xl={3}>
               <MiniStatisticsCard
                 title={{ text: "This month's Expense" }}
                 count={{ color: "error", text: expense }}
-                icon={{ color: "info", component: <IoDocumentText size="22px" color="white" /> }}
+                icon={{
+                  color: "info",
+                  component: <IoDocumentText size="22px" color="white" />,
+                }}
               />
             </Grid>
             <Grid item xs={12} md={6} xl={3}>
@@ -166,6 +186,7 @@ function Dashboard() {
           </Grid>
         </VuiBox>
         <VuiBox mb={3}>
+          <InvoiceModal showModal={isOpen} closeModal={closeModal} info={states}/>
           <Grid container spacing="18px">
             <Grid item xs={12} lg={12} xl={5}>
               <WelcomeMark />
@@ -183,14 +204,19 @@ function Dashboard() {
             <Grid item xs={12} lg={6} xl={7}>
               <Card>
                 <VuiBox sx={{ height: "100%" }}>
-                  <VuiTypography variant="lg" color="white" fontWeight="bold" mb="5px">
+                  <VuiTypography
+                    variant="lg"
+                    color="white"
+                    fontWeight="bold"
+                    mb="5px"
+                  >
                     Sales Overview
                   </VuiTypography>
                   <VuiBox mt={4} mb={1}>
-                  {/* <VuiButton color="info" fullWidth onClick={onSubmit}>
+                    {/* <VuiButton color="info" fullWidth onClick={onSubmit}>
                     Forecast
                   </VuiButton> */}
-                </VuiBox>
+                  </VuiBox>
                   {/* <VuiBox display="flex" alignItems="center" mb="40px">
                     <VuiTypography variant="button" color="success" fontWeight="bold">
                       +5% more{" "}
@@ -228,13 +254,26 @@ function Dashboard() {
                       barChartOptions={barChartOptionsDashboard}
                     />
                   </VuiBox>
-                  <VuiTypography variant="lg" color="white" fontWeight="bold" mb="5px">
+                  <VuiTypography
+                    variant="lg"
+                    color="white"
+                    fontWeight="bold"
+                    mb="5px"
+                  >
                     Active Users
                   </VuiTypography>
                   <VuiBox display="flex" alignItems="center" mb="40px">
-                    <VuiTypography variant="button" color="success" fontWeight="bold">
+                    <VuiTypography
+                      variant="button"
+                      color="success"
+                      fontWeight="bold"
+                    >
                       (+23){" "}
-                      <VuiTypography variant="button" color="text" fontWeight="regular">
+                      <VuiTypography
+                        variant="button"
+                        color="text"
+                        fontWeight="regular"
+                      >
                         than last week
                       </VuiTypography>
                     </VuiTypography>
@@ -251,18 +290,35 @@ function Dashboard() {
                           display="flex"
                           justifyContent="center"
                           alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
+                          sx={{
+                            borderRadius: "6px",
+                            width: "25px",
+                            height: "25px",
+                          }}
                         >
                           <IoWallet color="#fff" size="12px" />
                         </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
+                        <VuiTypography
+                          color="text"
+                          variant="button"
+                          fontWeight="medium"
+                        >
                           Users
                         </VuiTypography>
                       </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
+                      <VuiTypography
+                        color="white"
+                        variant="lg"
+                        fontWeight="bold"
+                        mb="8px"
+                      >
                         32,984
                       </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
+                      <VuiProgress
+                        value={60}
+                        color="info"
+                        sx={{ background: "#2D2E5F" }}
+                      />
                     </Grid>
                     <Grid item xs={6} md={3} lg={3}>
                       <Stack
@@ -275,18 +331,35 @@ function Dashboard() {
                           display="flex"
                           justifyContent="center"
                           alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
+                          sx={{
+                            borderRadius: "6px",
+                            width: "25px",
+                            height: "25px",
+                          }}
                         >
                           <IoIosRocket color="#fff" size="12px" />
                         </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
+                        <VuiTypography
+                          color="text"
+                          variant="button"
+                          fontWeight="medium"
+                        >
                           Clicks
                         </VuiTypography>
                       </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
+                      <VuiTypography
+                        color="white"
+                        variant="lg"
+                        fontWeight="bold"
+                        mb="8px"
+                      >
                         2,42M
                       </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
+                      <VuiProgress
+                        value={60}
+                        color="info"
+                        sx={{ background: "#2D2E5F" }}
+                      />
                     </Grid>
                     <Grid item xs={6} md={3} lg={3}>
                       <Stack
@@ -299,18 +372,35 @@ function Dashboard() {
                           display="flex"
                           justifyContent="center"
                           alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
+                          sx={{
+                            borderRadius: "6px",
+                            width: "25px",
+                            height: "25px",
+                          }}
                         >
                           <FaShoppingCart color="#fff" size="12px" />
                         </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
+                        <VuiTypography
+                          color="text"
+                          variant="button"
+                          fontWeight="medium"
+                        >
                           Sales
                         </VuiTypography>
                       </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
+                      <VuiTypography
+                        color="white"
+                        variant="lg"
+                        fontWeight="bold"
+                        mb="8px"
+                      >
                         2,400$
                       </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
+                      <VuiProgress
+                        value={60}
+                        color="info"
+                        sx={{ background: "#2D2E5F" }}
+                      />
                     </Grid>
                     <Grid item xs={6} md={3} lg={3}>
                       <Stack
@@ -323,18 +413,35 @@ function Dashboard() {
                           display="flex"
                           justifyContent="center"
                           alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
+                          sx={{
+                            borderRadius: "6px",
+                            width: "25px",
+                            height: "25px",
+                          }}
                         >
                           <IoBuild color="#fff" size="12px" />
                         </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
+                        <VuiTypography
+                          color="text"
+                          variant="button"
+                          fontWeight="medium"
+                        >
                           Items
                         </VuiTypography>
                       </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
+                      <VuiTypography
+                        color="white"
+                        variant="lg"
+                        fontWeight="bold"
+                        mb="8px"
+                      >
                         320
                       </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
+                      <VuiProgress
+                        value={60}
+                        color="info"
+                        sx={{ background: "#2D2E5F" }}
+                      />
                     </Grid>
                   </Grid>
                 </VuiBox>
@@ -348,3 +455,4 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
